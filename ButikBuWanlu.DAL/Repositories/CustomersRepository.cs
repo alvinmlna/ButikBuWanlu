7e2be090a49d2d15811f2942cf61ab44d9aaa28c
Alvin maulana <alvinmaulananrs@gmail.com>
2022-01-28 23:35:05 +0700

    implement repository

---
+++ ButikBuWanlu.DAL/Repositories/CustomersRepository.cs
@@ -0,0 +1,17 @@
+﻿using ButikBuWanlu.Domain.Absctraction.IRepository;
+using ButikBuWanlu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikBuWanlu.DAL.Repositories
+{
+    internal class CustomersRepository : Repository<Customer>, ICustomersRepository
+    {
+        internal CustomersRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ ButikBuWanlu.DAL/Repositories/StoresRepository.cs
@@ -0,0 +1,17 @@
+﻿using ButikBuWanlu.Domain.Absctraction.IRepository;
+using ButikBuWanlu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikBuWanlu.DAL.Repositories
+{
+    internal class StoresRepository : Repository<Store>, IStoresRepository
+    {
+        internal StoresRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
