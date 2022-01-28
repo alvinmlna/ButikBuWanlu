8aa9670e148559153ac88070ba441fa7fcd79df1
Alvin maulana <alvinmaulananrs@gmail.com>
2022-01-28 23:05:51 +0700

    add unit of work

---
+++ ButikBuWanlu.Domain/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using ButikBuWanlu.Domain.Absctraction.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikBuWanlu.Domain
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        #region properties
+
+        ICustomersRepository CustomersRepository { get; }
+        IItemsRepository ItemsRepository { get; }
+        IStoresRepository StoresRepository { get; }
+        ITransactionsRepository TransactionsRepository { get; }
+
+        #endregion
+
+        #region methods
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        #endregion
+    }
+}
